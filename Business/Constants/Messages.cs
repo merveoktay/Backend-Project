00fe62a02b2958b4faf88d0fd983f9a37f7ccc9a
Merve Oktay <merve.oktay@hotmail.com>
2021-05-04 19:27:03 +0300

    CSharp Backend Project

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+	public class Messages
+	{
+		public static string ProductAdded = "Ürün Eklendi";
+		public static string ProductNameInvalid = "Ürün İsmi Geçersiz";
+		public static string MaintenanceTime = "Sistem Bakımda";
+		public static string ProductsListed = "Ürünler Listelendi";
+		public static string ProductCountOfCategoryError = "Bir Kategoride En Fazla 10 Ürün Olabilir";
+		public static string ProductNameAlreadyExists = "Aynı İsimde Ürün Olamaz";
+		public static string CategoryLimitExceded = "Kategory Sınırı Aşıldı";
+		public static string AuthorizationDenied = "Yetkiniz Yok";
+		public static string UserRegistered= "Hata Var";
+		public static string UserNotFound= "Hata Var";
+		public static string PasswordError= "Hata Var";
+		public static string SuccessfulLogin= "Hata Var";
+		public static string UserAlreadyExists="Kullanıcı Mevcut";
+		public static string AccessTokenCreated="Hata Var";
+	}
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+	public interface IEntityRepository<T> where T:class,IEntity,new()//Entities nesnelerini alması için yaptık
+	{
+		List<T> GetAll(Expression<Func<T,bool>>filter=null);//filtreleyerek listeleme işlemi için kullanılır null boş da olsa olur demek
+		T Get(Expression<Func<T, bool>> filter );
+		void Add(T entity); 
+		void Update(T entity);
+		void Delete(T entity);
+
+	} 
+}
